5930593c9a8d666abb1b7d5051ee7fd1c5627aef
Balázs Indig <indba@digitus.itk.ppke.hu>
2017-05-08 13:20:49 +0200

    Step two

--- filters/token_filter.py
+++ filters/token_filter.py
@@ -90,6 +90,19 @@ class TokenFilter:
             result.append(copy)
         return result
 
+    def select_token(self, token):
+        # Linear search: For every property x For every allowed 'string'
+        # Index poperty is in range or full or partial stringmatch
+        for p in token.get_property_types():
+            prop_name = p.name
+            prop = token.get_property(p)
+            for allowed in self._allowed_strings:
+                if (prop_name == "Index" and isinstance(allowed, range) and int(prop) in allowed) or \
+                   (not isinstance(allowed, range) and (self._whole_word and prop == allowed or
+                                                        not self._whole_word and allowed in prop)):
+                    return True
+        return False
+
     def filter(self, original: NLPInstance):
         """Filter an NLP instance.
 
@@ -108,23 +121,17 @@ class TokenFilter:
             updated_split_points = None
         else:
             # first filter out tokens not containing allowed strings
+
             old2new = {}  # HashMap<Token, Token>()
             new2old = {}  # HashMap<Token, Token>()
             updated_tokens = []  # ArrayList<Token>()
-            for token in original.tokens:  # Linear search: For every property x For every allowed 'string'
-                for prop, prop_name, allowed in \
-                    ((token.get_property(p), p.name, allowed) for p in token.get_property_types()
-                     for allowed in self._allowed_strings):
-                    # Index poperty is in range or full or partial stringmatch
-                    if (prop_name == "Index" and isinstance(allowed, range) and int(prop) in allowed) or \
-                            (not isinstance(allowed, range) and (self._whole_word and prop == allowed or
-                                                                 not self._whole_word and allowed in prop)):
-                        new_token = Token(len(updated_tokens))
-                        new_token.merge(original.tokens[token.index])
-                        old2new[token] = new_token
-                        new2old[new_token] = token
-                        updated_tokens.append(new_token)
-                        break
+            for token in (t for t in original.tokens if self.select_token(t)):
+                new_token = Token(len(updated_tokens))
+                new_token.merge(original.tokens[token.index])
+                old2new[token] = new_token
+                new2old[new_token] = token
+                updated_tokens.append(new_token)
+
             # update edges and remove those that have vertices not in the new vertex set
             updated_edges = set()  # ArrayList<Edge>()
             for e in (e for e in original.get_edges() if e.start in old2new and e.end in old2new):
