6d8932f88984ebdcb48143da20173eb752e68245
Andras Simonyi <andras.simonyi@analogy.co>
2017-05-15 18:47:02 +0200

    Remove some superflous attributes from NLPCanvas

--- NLPCanvas.py
+++ NLPCanvas.py
@@ -27,15 +27,13 @@ class NLPCanvas:
         self.renderer = SingleSentenceRenderer()
         self.renderers = {RenderType.single: SingleSentenceRenderer(),
                           RenderType.alignment: AligmentRenderer()}
-        self.tokens = []
-        self.dependencies = set()
         self.usedTypes = set()
         self.usedProperties = set()
         self.filter = None
-        self._ui = ui
-        self._nlp_instance = None
-        self._listeners = []
-        self._changeListeners = []
+        self.ui = ui
+        self.nlp_instance = None
+        self.listeners = []
+        self.change_listeners = []
 
     def addListener(self, listener):
         """
@@ -43,7 +41,7 @@ class NLPCanvas:
          *
          * @param listener the listener to add.
         """
-        self._listeners.append(listener)
+        self.listeners.append(listener)
 
     def addChangeListener(self, change_listener):
         """
@@ -51,20 +49,20 @@ class NLPCanvas:
          *
          * @param changeListener the listener to add.
         """
-        self._changeListeners.append(change_listener)
+        self.change_listeners.append(change_listener)
 
     def fireChanged(self):
         """
          * Fired whenever this canvas is changed.
         """
-        for changeListener in self._changeListeners:
+        for changeListener in self.change_listeners:
             changeListener.stateChanged()
 
     def fireInstanceChanged(self):
         """
          * Notifies all listeners about an instance change event.
         """
-        for l in self._listeners:
+        for l in self.listeners:
             l.instanceChanged()
 
     def set_nlp_instance(self, nlp_instance):
@@ -74,11 +72,9 @@ class NLPCanvas:
          *
          * @param nlpInstance the new NLP instance.
         """
-        self._nlp_instance = nlp_instance
-        self.dependencies = self._nlp_instance.get_edges()
-        self.usedTypes = {edge.edge_type for edge in self.dependencies}  # Union
-        self.tokens = self._nlp_instance.tokens
-        self.usedProperties = {prop for token in self.tokens for prop in token.get_properties()}  # UnionAll
+        self.nlp_instance = nlp_instance
+        self.usedTypes = {edge.edge_type for edge in self.nlp_instance.get_edges()}  # Union
+        self.usedProperties = {prop for token in self.nlp_instance.tokens for prop in token.get_properties()}  # UnionAll
         self.fireInstanceChanged()
 
     def filter_instance(self):
@@ -87,12 +83,7 @@ class NLPCanvas:
         Returns:
             NLPInstance: The filtered instance.
         """
-        # print('Call to filter_instance')
-        instance = NLPInstance(tokens=self.tokens, edges=self.dependencies,
-                               render_type=self._nlp_instance.render_type,
-                               split_points=self._nlp_instance.split_points)
-
-        return self.filter.filter(instance)
+        return self.filter.filter(self.nlp_instance)
 
     def update_nlp_graphics(self):
         """Updates the current graph.
@@ -102,18 +93,18 @@ class NLPCanvas:
         """
         print('NLPCanvas#updateNLPGraphics')
         scene = QtGui.QGraphicsScene()
-        self._ui.graphicsView.setScene(scene)
+        self.ui.graphicsView.setScene(scene)
         br = QtSvg.QGraphicsSvgItem()
         rr = QtSvg.QSvgRenderer(Scene.export_nlp_graphics(self.renderer, self.filter_instance()))
         br.setSharedRenderer(rr)
         scene.addItem(br)
-        self._ui.graphicsView.show()
+        self.ui.graphicsView.show()
         self.fireChanged()
 
     def clear(self):
         """Clears the current instance.
         """
-        self.tokens.clear()
-        self.dependencies.clear()
+        self.nlp_instance.tokens = []
+        self.nlp_instance.edges = []
         self.usedTypes.clear()
         self.usedProperties.clear()
