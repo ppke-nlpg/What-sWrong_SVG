ppke-nlpg/What-sWrong_SVG

commit 2bc728f7ce88970b30d41065265f62e138cfc66d

Kill TokenProperty class and reorder the functions of NLPInstance class

libwwnlp/render/token_layout.py
@@ -71,8 +71,7 @@ class TokenLayout:
         self.width = 0
         self.height = 0
 
-    def estimate_token_bounds(self, instance: NLPInstance, token_widths: dict,
-                              scene):
+    def estimate_token_bounds(self, instance: NLPInstance, token_widths: dict, scene):
         """Calculate the horizontal bounds of each token in the layout of the tokens.
 
         Args:
@@ -111,9 +110,8 @@ class TokenLayout:
             token = tokens[token_index]
             maxx = 0
             lasty = self.base_line + self.row_height
-            for prop in token.get_sorted_properties():
-                curr_property = token.get_property(prop)
-                labelwidth = Text(scene, (0, 0), curr_property, 12).get_width()
+            for prop_name in token.get_sorted_properties():
+                labelwidth = Text(scene, (0, 0), token.get_property(prop_name), 12).get_width()
                 lasty += self.row_height
                 if labelwidth > maxx:
                     maxx = labelwidth
@@ -165,16 +163,17 @@ class TokenLayout:
             index = 0
             lasty = self.base_line
             maxx = 0
-            for prop in token.get_sorted_properties():
+            for prop_name in token.get_sorted_properties():
                 lasty += self.row_height
-                curr_property = token.get_property(prop)
+                curr_property_value = token.get_property(prop_name)
                 if index == 0:
                     token_color = (0, 0, 0)  # Black
                 else:
                     token_color = (120, 120, 120)  # Grey
-                scene.add(TextToken(scene, (lastx, lasty), curr_property, 12, token_color))
-                maxx = max(maxx, Text(scene, (0, 0), curr_property, 12, token_color).get_width())
-                self.text_layouts[(token, index+1)] = curr_property
+                text_token = TextToken(scene, (lastx, lasty), curr_property_value, 12, token_color)
+                scene.add(text_token)
+                maxx = max(maxx, text_token.get_width())
+                self.text_layouts[(token, index+1)] = curr_property_value
                 index += 1
             required_width = token_widths.get(token)
             if required_width is not None and maxx < required_width:

CorpusNavigator.py
@@ -5,7 +5,6 @@
 from NLPCanvas import NLPCanvas
 from CorpusLoader import CorpusLoader
 from libwwnlp.model.nlp_instance import NLPInstance, nlp_diff
-from libwwnlp.model.token_property import TokenProperty
 
 """
  * A CorpusNavigator allows the user to navigate through a corpus (or a diffed corpus) and pick one NLP instance to draw
@@ -539,7 +538,7 @@ class CorpusNavigator:
             instance = self._indices[index]
             sentence = ""
             for token in instance.tokens:
-                word = token.get_property_by_name("Word")
+                word = token.get_property("Word")
                 if sentence == "":
                     sentence += " " + word
                 else:
@@ -689,12 +688,12 @@ class CorpusNavigator:
             """
 
             example = NLPInstance()
-            example.add_token().add_named_prop("Word", "[root]").add_named_prop("Index", "0")
-            example.add_token().add_named_prop("Word", "Add").add_named_prop("Index", "1")
-            example.add_token().add_named_prop("Word", "a").add_named_prop("Index", "2")
-            example.add_token().add_named_prop("Word", "gold").add_named_prop("Index", "3")
-            example.add_token().add_named_prop("Word", "corpus").add_named_prop("Index", "4")
-            example.add_token().add_named_prop("Word", "!").add_named_prop("Index", "5")
+            example.add_token().add_property("Word", "[root]").add_property("Index", "0")
+            example.add_token().add_property("Word", "Add").add_property("Index", "1")
+            example.add_token().add_property("Word", "a").add_property("Index", "2")
+            example.add_token().add_property("Word", "gold").add_property("Index", "3")
+            example.add_token().add_property("Word", "corpus").add_property("Index", "4")
+            example.add_token().add_property("Word", "!").add_property("Index", "5")
             example.add_dependency(0, 1, "ROOT", "dep")
             example.add_dependency(0, 5, "PUNC", "dep")
             example.add_dependency(1, 4, "OBJ", "dep")

NLPCanvas.py
@@ -83,7 +83,7 @@ class NLPCanvas:
         """
         self.nlp_instance = nlp_instance
         self.usedTypes = {edge.edge_type for edge in self.nlp_instance.get_edges()}
-        self.usedProperties = {prop for token in self.nlp_instance.tokens for prop in token.get_properties()}
+        self.usedProperties = {prop for token in self.nlp_instance.tokens for prop in token.get_sorted_properties()}
         self.used_edge_properties = set()
         for edge in self.nlp_instance.get_edges():
             self.used_edge_properties.update(edge.properties)

ioFormats/TabProcessor.py
@@ -9,7 +9,6 @@ import sys
 import re
 
 from libwwnlp.model.nlp_instance import NLPInstance
-from libwwnlp.model.token_property import TokenProperty
 from ioFormats.CorpusFormat import CorpusFormat
 
 """
@@ -262,8 +261,8 @@ class CoNLL2000:
         for row in rows:
             chunk = row[2]
             instance.add_token().\
-                add_named_prop(name="Word", value=row[0]).\
-                add_named_prop(name="Index", value=str(index))
+                add_property(name="Word", value=row[0]).\
+                add_property(name="Index", value=str(index))
             instance.add_span(index, index, row[1], "pos")
             instance.add_span(index, index, chunk, "chunk (BIO)")
             index += 1
@@ -334,8 +333,8 @@ class CoNLL2002:
         for row in rows:
             row = row.strip().split()
             instance.add_token().\
-                add_named_prop(name="Word", value=row[0]).\
-                add_named_prop(name="Index", value=str(index))
+                add_property(name="Word", value=row[0]).\
+                add_property(name="Index", value=str(index))
             instance.add_span(index, index, row[1], "ner (BIO)")
             index += 1
 
@@ -405,8 +404,8 @@ class CoNLL2003:
         for row in rows:
             row = row.strip().split()
             instance.add_token().\
-                add_named_prop(name="Word", value=row[0]).\
-                add_named_prop(name="Index", value=str(index))
+                add_property(name="Word", value=row[0]).\
+                add_property(name="Index", value=str(index))
 
             instance.add_span(index, index, row[1], "pos")
             instance.add_span(index, index, row[2], "chunk (BIO")
@@ -479,8 +478,8 @@ class CoNLL2004:
         for row in rows:
             row = row.strip().split()
             instance.add_token().\
-                add_named_prop(name="Word", value=row[0]).\
-                add_named_prop(name="Index", value=str(index))
+                add_property(name="Word", value=row[0]).\
+                add_property(name="Index", value=str(index))
             index += 1
         predicateCount = 0
         index = 0
@@ -557,8 +556,8 @@ class CoNLL2005:
         for row in rows:
             row = row.strip().split()
             instance.add_token().\
-                add_named_prop(name="Word", value=row[0]).\
-                add_named_prop(name="Index", value=str(index))
+                add_property(name="Word", value=row[0]).\
+                add_property(name="Index", value=str(index))
             index += 1
         predicateCount = 0
         index = 0
@@ -632,16 +631,16 @@ class CoNLL2006:
     @staticmethod  # XXX Currently static but maybe later it will be changed...
     def create(rows):
         instance = NLPInstance()
-        instance.add_token().add_named_prop("Word", "-Root-")
+        instance.add_token().add_property("Word", "-Root-")
         for row in rows:
             row = row.strip().split()
             instance.add_token().\
-                add_named_prop(name="Word", value=row[1]).\
-                add_named_prop(name="Index", value=row[0]).\
-                add_named_prop(name="Lemma", value=row[2]).\
-                add_named_prop(name="CPos", value=row[3]).\
-                add_named_prop(name="Pos", value=row[4]).\
-                add_named_prop(name="Feats", value=row[5])
+                add_property(name="Word", value=row[1]).\
+                add_property(name="Index", value=row[0]).\
+                add_property(name="Lemma", value=row[2]).\
+                add_property(name="CPos", value=row[3]).\
+                add_property(name="Pos", value=row[4]).\
+                add_property(name="Feats", value=row[5])
         for row in rows:
             row = row.strip().split()
             # dependency
@@ -650,7 +649,7 @@ class CoNLL2006:
                 instance.add_dependency(start=int(row[6]), end=mod, label=row[7], dep_type="dep")
             except:  # XXX TRACK DOWN POSSIBLE EXCEPTION TYPES!
                 print("Can't parse dependency", file=sys.stderr)
-                instance.tokens[mod].add_named_prop("DepMissing", "missing")
+                instance.tokens[mod].add_property("DepMissing", "missing")
             # role
         return instance
 
@@ -710,18 +709,18 @@ class CoNLL2008:
     @staticmethod  # XXX Currently static but maybe later it will be changed...
     def create(rows):
         instance = NLPInstance()
-        instance.add_token().add_named_prop("Word", "-Root-")
+        instance.add_token().add_property("Word", "-Root-")
         predicates = []  # ArrayList<Integer>()
         for row in rows:
             row = row.strip().split()
             instance.add_token().\
-                add_named_prop(name="Word", value=row[1]).\
-                add_named_prop(name="Index", value=row[0]).\
-                add_named_prop(name="Lemma", value=row[2]).\
-                add_named_prop(name="Pos", value=row[3]).\
-                add_named_prop(name="Split Form", value=row[5]).\
-                add_named_prop(name="Split Lemma", value=row(6)).\
-                add_named_prop(name="Split PoS", value=row[7])
+                add_property(name="Word", value=row[1]).\
+                add_property(name="Index", value=row[0]).\
+                add_property(name="Lemma", value=row[2]).\
+                add_property(name="Pos", value=row[3]).\
+                add_property(name="Split Form", value=row[5]).\
+                add_property(name="Split Lemma", value=row(6)).\
+                add_property(name="Split PoS", value=row[7])
             if row[10] != "_":
                 index = int(row[0])
                 predicates.append(index)
@@ -755,9 +754,9 @@ class CoNLL2008:
         for row in rows:
             row = row.strip().split()
             instance.add_token(). \
-                add_property("Named Entity", 10, row[0]). \
-                add_property("NamedEntity BBN", 11, value=row[1]). \
-                add_property("WordNet", 12, value=row[2])
+                add_property("Named Entity", row[0], 10). \
+                add_property("NamedEntity BBN", row[1], 11). \
+                add_property("WordNet", row[2], 12)
         index = 1
         for row in rows:
             row = row.strip().split()
@@ -823,19 +822,19 @@ class CoNLL2009:
     @staticmethod  # XXX Currently static but maybe later it will be changed...
     def create(rows):
         instance = NLPInstance()
-        instance.add_token().add_named_prop(name="Word", value="-Root-")
+        instance.add_token().add_property(name="Word", value="-Root-")
         predicates = []  # ArrayList<Integer>()
         for row in rows:
             row = row.strip().split()
             instance.add_token().\
-                add_named_prop(name="Word", value=row[1]).\
-                add_named_prop(name="Index", value=row[0]).\
-                add_named_prop(name="Lemma", value=row[2]).\
-                add_named_prop(name="PLemma", value=row[3]).\
-                add_named_prop(name="PoS", value=row[4]).\
-                add_named_prop(name="PPoS", value=row[5]).\
-                add_named_prop(name="Feat", value=row[6]).\
-                add_named_prop(name="PFeat", value=row[7])
+                add_property(name="Word", value=row[1]).\
+                add_property(name="Index", value=row[0]).\
+                add_property(name="Lemma", value=row[2]).\
+                add_property(name="PLemma", value=row[3]).\
+                add_property(name="PoS", value=row[4]).\
+                add_property(name="PPoS", value=row[5]).\
+                add_property(name="Feat", value=row[6]).\
+                add_property(name="PFeat", value=row[7])
             if row[13] != "_":
                 index = int(row[0])
                 predicates.append(index)
@@ -914,14 +913,14 @@ class MaltTab:
     @staticmethod  # XXX Currently static but maybe later it will be changed...
     def create(rows):
         instance = NLPInstance()
-        instance.add_token().add_named_prop(name="Word", value="-Root-")
+        instance.add_token().add_property(name="Word", value="-Root-")
         index = 1
         for row in rows:
             row = row.strip().split()
             instance.add_token().\
-                add_named_prop(name="Word", value=row[0]).\
-                add_named_prop(name="Index", value=str(index)).\
-                add_named_prop(name="Pos", value=row[1])
+                add_property(name="Word", value=row[0]).\
+                add_property(name="Index", value=str(index)).\
+                add_property(name="Pos", value=row[1])
             index += 1
         mod = 1
         for row in rows:
@@ -931,7 +930,7 @@ class MaltTab:
                 instance.add_dependency(start=int(row[2]), end=mod, label=row[3], dep_type="dep")
             except:  # XXX TRACK DOWN POSSIBLE EXCEPTION TYPES!
                 print("Can't parse dependency", file=sys.stderr)
-                instance.tokens[mod].add_named_prop("DepMissing", "missing")
+                instance.tokens[mod].add_property("DepMissing", "missing")
             # role
             mod += 1
         return instance
@@ -998,11 +997,11 @@ class CCG:
         for i in range(2, len(sentence)):
             w_t_c = sentence[i].split("|")  # In Python this is not regex
             instance.add_token().\
-                add_named_prop(name="Word", value=w_t_c[0]).\
-                add_named_prop(name="Tag", value=w_t_c[1]).\
-                add_named_prop(name="Category", value=w_t_c[2]).\
-                add_named_prop(name="Index", value=str(i-1))
-        # instance.add_token().add_named_prop("Word", "-Root-")
+                add_property(name="Word", value=w_t_c[0]).\
+                add_property(name="Tag", value=w_t_c[1]).\
+                add_property(name="Category", value=w_t_c[2]).\
+                add_property(name="Index", value=str(i - 1))
+        # instance.add_token().add_property("Word", "-Root-")
 
         mod = 1
         for row in rows:
@@ -1013,7 +1012,7 @@ class CCG:
                     instance.add_edge(start=int(row[1]), end=int(row[0]), label=row[2] + "_" + row[3], edge_type="dep")
                 except:  # XXX TRACK DOWN POSSIBLE EXCEPTION TYPES!
                     print("Can't parse dependency", file=sys.stderr)
-                    instance.tokens[mod].add_named_prop("DepMissing", "missing")
+                    instance.tokens[mod].add_property("DepMissing", "missing")
                 mod += 1
         return instance
 

libwwnlp/model/filter.py
@@ -6,7 +6,6 @@ from operator import attrgetter
 
 from ..model.nlp_instance import NLPInstance
 from ..model.token import Token
-from ..model.token_property import TokenProperty
 from ..model.edge import Edge, EdgeRenderType
 
 
@@ -72,7 +71,7 @@ class Filter:
                                        'eval_status_FP',
                                        'eval_status_Match'}
         self.forbidden_token_properties = set()
-        self.allowed_token_propvals = allowed_token_propvals
+        self.allowed_token_propvals = allowed_token_propvals or {''}
         self.propvals_whole_word = False
         self.use_path = False
         self.collapse = False
@@ -145,8 +144,8 @@ class Filter:
         Returns:
             bool: True iff the token should be kept.
         """
-        for prop_name in token.get_properties():
-            prop_val = token.get_property_by_name(prop_name)
+        for prop_name in token.get_sorted_properties():
+            prop_val = token.get_property(prop_name)
             if len(self.allowed_token_propvals) > 0:
                 for allowed in self.allowed_token_propvals:
                     if (prop_name == "Index" and isinstance(allowed, range) and int(prop_val) in allowed) or \

commit a2c17bc954d9db2c331753e64c01ddb1af1cfe04

Fix token length issue
Introduced in 2bc728f7ce88970b30d41065265f62e138cfc66d

libwwnlp/render/token_layout.py
@@ -167,7 +167,7 @@ class TokenLayout:
                     token_color = (120, 120, 120)  # Grey  # TODO: Constants?
                 text_token = TextToken(scene, (lastx, lasty), curr_property_value, 12, token_color)  # TODO: Constants?
                 scene.add(text_token)
-                maxx = max(maxx, text_token.get_width())
+                maxx = max(maxx, Text(scene, (0, 0), curr_property_value, 12, token_color).get_width())
                 self.text_layouts[(token, index+1)] = curr_property_value
                 index += 1
             required_width = token_widths.get(token)